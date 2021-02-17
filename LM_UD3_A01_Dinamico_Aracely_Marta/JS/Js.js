2fd0e93e1ec6a532eb416762e26d0343909df9e0
Aracely Berrocal <73697069+aracelyberrocal@users.noreply.github.com>
2021-02-17 09:32:56 +0100

    cambios cely javascript

--- LM_UD3_A01_Dinamico_Aracely_Marta/JS/Js.js
+++ LM_UD3_A01_Dinamico_Aracely_Marta/JS/Js.js
@@ -124,4 +124,13 @@ function cambcuadrado10() {
     document.getElementById("cuadrado10").style.backgroundColor = arrayColor[aux];
     iguales();
 }
+//falta arreglar
+function pulsar(){
+    var x= document.getElementById('lista')
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
 
+}
